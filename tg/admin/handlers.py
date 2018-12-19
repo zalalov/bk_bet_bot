5336dd2d954ee0dc2179b897b5d6609835f3d787
Ruslan Zalalov <zalalov@mail.ru>
2018-12-20 01:02:10 +0300

    New handlers for admin bot.

--- tg/admin/handlers.py
+++ tg/admin/handlers.py
@@ -1,13 +1,62 @@
 import config
-from tg.admin.markup import markup_main
+from tg.decorators import is_user_admin, is_admin_mode
+from tg.admin.markup import (
+    markup_main,
+    markup_bets,
+    markup_users,
+    markup_back
+)
+from tg.admin.markup import (
+    BUTTON_USERS,
+    BUTTON_BETS,
+    BUTTON_USER_ADD,
+    BUTTON_USER_LIST,
+    BUTTON_BET_LIST,
+    BUTTON_BET_ADD,
+    BUTTON_BACK,
+)
 from tg.bot import bot
 
 conf = config.get_config()
 
 
-@bot.message_handler(func=lambda message: conf.is_admin_mode())
-@bot.message_handler(func=lambda message: conf.is_user_admin(message.chat.id))
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
-    print(message.from_user.username)
+@is_admin_mode
+@is_user_admin
+def main_menu(message):
     bot.send_message(message.chat.id, 'Выбери действие:', reply_markup=markup_main())
+
+
+@bot.message_handler(func=lambda m: m.text == BUTTON_USERS)
+@is_admin_mode
+@is_user_admin
+def users(message):
+    bot.send_message(message.chat.id, 'USERS HANDLER', reply_markup=markup_users())
+
+
+@bot.message_handler(func=lambda m: m.text == BUTTON_USER_LIST)
+@is_admin_mode
+@is_user_admin
+def users(message):
+    bot.send_message(message.chat.id, 'USER LIST HANDLER', reply_markup=markup_back())
+
+
+@bot.message_handler(func=lambda m: m.text == BUTTON_BETS)
+@is_admin_mode
+@is_user_admin
+def users(message):
+    bot.send_message(message.chat.id, 'BETS HANDLER', reply_markup=markup_bets())
+
+
+@bot.message_handler(func=lambda m: m.text == BUTTON_BET_LIST)
+@is_admin_mode
+@is_user_admin
+def users(message):
+    bot.send_message(message.chat.id, 'BET LIST HANDLER', reply_markup=markup_back())
+
+
+@bot.message_handler(func=lambda m: m.text == BUTTON_BACK)
+@is_admin_mode
+@is_user_admin
+def users(message):
+    main_menu(message)
